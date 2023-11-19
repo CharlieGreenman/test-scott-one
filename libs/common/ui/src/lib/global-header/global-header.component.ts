24f07a267e41fd865e21bd0429c480b8cc38b8e6
Charlie Greenman <charlie@razroo.com>
2023-11-19 12:07:20 -0500

    Add code for auth-0-angular-16.2.0 add-header-authentication-buttons.
    [Url: razroo.com/razroo/auth-0-angular-16.2.0/add-login-ui/add-header-authentication-buttons]

--- libs/common/ui/src/lib/global-header/global-header.component.ts
+++ libs/common/ui/src/lib/global-header/global-header.component.ts
@@ -6,6 +6,7 @@ import { FontAwesomeModule } from "@fortawesome/angular-fontawesome";
 import { MatButtonModule } from '@angular/material/button';
 import { faBell, faQuestionCircle, faUserCircle } from '@fortawesome/free-solid-svg-icons';
 import { Input } from "@angular/core";
+import { Input } from "@angular/core";
 
 @Component({
   selector: 'test-scott-one-global-header',
@@ -13,6 +14,7 @@ import { Input } from "@angular/core";
   styleUrls: ['./global-header.component.scss'],
 })
 export class GlobalHeaderComponent implements OnInit {
+  @Input() authenticated = false;
   @Output() signUp: EventEmitter<void> = new EventEmitter();
   @Output() logIn: EventEmitter<void> = new EventEmitter();
   @Input() authenticated = false;
